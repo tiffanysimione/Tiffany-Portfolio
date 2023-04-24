a5b438b8f3870b4ffdf0ff9e38da07b034ff0507
tiffanysimione <simionetiffany1@gmail.com>
2023-04-24 12:09:58 -0400

    first draft trying to go live

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/logo.jpeg'
+import {Link} from 'react-scroll'
 
 const NavBar =() => {
  // toggle hamburger menu on click
@@ -18,11 +19,21 @@ const handleClick =() => setNav(!nav)
 
 {/* menu */}
 <ul className='hidden md:flex'>
-    <li>Home</li>
-    <li>About</li>
-    <li>Skills</li>
-    <li>Work</li>
-    <li>Contact</li>
+    <li>
+    <Link to="home" smooth={true} duration={500} > Home</Link>
+    </li>
+    <li>
+    <Link to="about" smooth={true} duration={500} > About</Link>
+    </li>
+    <li>
+    <Link to="skills" smooth={true} duration={500} > Skills</Link>
+    </li>
+    <li>
+    <Link to="work" smooth={true} duration={500} > Work</Link>
+    </li>
+    <li>
+    <Link to="contact" smooth={true} duration={500} > Contact</Link>
+    </li>
 </ul>
 
 
