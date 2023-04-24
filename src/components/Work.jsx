514a8544f349b3115dad40ba64f2116b468833c2
tiffanysimione <simionetiffany1@gmail.com>
2023-04-24 16:53:17 -0400

    changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ComingSoon from '../assets/ComingSoon.png'
+import trying from '../assets/trying.png'
 
 function Work() {
   return (
@@ -14,7 +14,7 @@ function Work() {
 
 <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-4'>
       {/* Grid Item */}
-<div style ={{backgroundImage: `url(${ComingSoon})`}}
+<div style ={{backgroundImage: `url(${trying})`}}
 className ='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
     {/* Hover Effects */}
@@ -31,7 +31,24 @@ React JS Application
     </div>
 </div>
       {/* Grid Item */}
-      <div style ={{backgroundImage: `url(${ComingSoon})`}}
+      <div style ={{backgroundImage: `url(${trying})`}}
+className =' comingsoo shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+    {/* Hover Effects */}
+
+    <div className='opacity-0 group-hover:opacity-100'>
+    <span className='text-2xl font-bold text-white tracking-wider'>
+React JS Application
+    </span>
+    <div className='pt-8 text-center'>
+        <a href='/'>
+            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-green-500 font-bold text-lg'>Demo</button> </a>
+        <a href='/'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-green-500 font-bold text-lg'>Code</button> </a>
+    </div>
+    </div>
+</div>
+      {/* Grid Item */}
+      <div style ={{backgroundImage: `url(${trying})`}}
 className ='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
     {/* Hover Effects */}
@@ -48,24 +65,7 @@ React JS Application
     </div>
 </div>
       {/* Grid Item */}
-      <div style ={{backgroundImage: `url(${ComingSoon})`}}
-className ='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-    {/* Hover Effects */}
-
-    <div className='opacity-0 group-hover:opacity-100'>
-    <span className='text-2xl font-bold text-white tracking-wider'>
-React JS Application
-    </span>
-    <div className='pt-8 text-center'>
-        <a href='/'>
-            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-green-500 font-bold text-lg'>Demo</button> </a>
-        <a href='/'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-green-500 font-bold text-lg'>Code</button> </a>
-    </div>
-    </div>
-</div>
-      {/* Grid Item */}
-      <div style ={{backgroundImage: `url(${ComingSoon})`}}
+      <div style ={{backgroundImage: `url(${trying})`}}
 className ='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
     {/* Hover Effects */}
