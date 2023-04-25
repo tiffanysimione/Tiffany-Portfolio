2b12c7c5338d364fc8ba7d262e6e3bb527da78bf
tiffanysimione <simionetiffany1@gmail.com>
2023-04-25 14:03:24 -0400

    bug fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,28 +4,24 @@ import airplane from '../assets/airplane.png'
 
 const Contact = () => {
   return (
-    <div name ='contact' className='w-full h-screen flex justify-center items-center p-4 pt-20'>
-
+    <div name ='contact' className='w-full h-screen flex justify-center items-center p-4'>
       <div className='contact-image-container '>
      
     <img src ={airplane} alt="airplane" style={{width:'50px'}}/>
    </div>
-
       
       <form method='POST' action="https://getform.io/f/2269e4c9-35b0-4bc9-b475-a75f7d418515" className='flex flex-col max-w-[600px] w-full'> 
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline '>Send me a message!</p>
+        <div className='pb-8 mt-20'>
+          <p className='text-4xl font-bold inline'>Send me a message!</p>
           <p className='py-4'>Submit the form below or shoot me an email - simionetiffany1@gmail.com</p>
         </div>
         <input className='p-2' type='text' placeholder='Enter your name' name='name' />
         <input className='my-4 p-2' type='email' placeholder='Enter your email' name='email' />
         <textarea className='p-2' name='message' rows='10' placeholder='Message'></textarea>
-        <button className='text-bg[#002d72] border-2 hover:bg-[#fa8072]-400 hover:border-[#text-[#fa8072] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#fa8072]-300 px-4 py-3 my-8 mx-auto flex items-center'> Let's Connect</button>
+        <button className='text-white border-2 hover:bg-pink-400 hover:border-pink-300 px-4 py-3 my-8 mx-auto flex items-center'> Let's Connect</button>
       </form>
     </div>
   )
 }
 
 export default Contact
-
-
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import trying from '../assets/trying.png'
 
 
+
 function Work() {
-    return (
-      <div name ='work' className='w-full md:h-screen pt-20'>
-        <div className='max-w-[100%] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div className='pb-8'> 
-            <p className='text-4xl font-bold inline'>Work</p>
-            <p className='py-6'>Check out some of my recent work</p>
-          </div>
+  return (
+<div name ='work' className='w-full md:h-screen'>
+<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div className='pb-8 mt-20'> 
+    <p className='text-4xl font-bold inline mt-4 '>Work</p>
+    <p className='py-6'>Check out some of my recent work</p>
+</div>
+
 
 <img src= 'https://uploads-ssl.webflow.com/6228e4b0518bd64c391affa4/622a3bfe16215b781d5cc69a_Wine%20glasses.gif'> 
 </img>
