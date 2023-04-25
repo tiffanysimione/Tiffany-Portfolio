d071076a32985c05c610fec5fa214d3854ec6221
tiffanysimione <simionetiffany1@gmail.com>
2023-04-25 14:13:32 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Work from './components/Work';
 import Contact from './components/Contact';
-import Welcome from './components/Welcome';
+// import Welcome from './components/Welcome';
 // import test from './components/test';
 
 function App() {
   return (
     <div className='flex flex-col items-center overflow-x-hidden background-color text-color;'>
       <NavBar/>
-      <Welcome  />
+      {/* <Welcome  /> */}
       <Home />
       <About />
       <Skills />
